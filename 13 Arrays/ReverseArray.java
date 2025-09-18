e96528ef4d9da52a8ebcd7c8d23896bb560beca3
Ashraf Alam <170826900+Ashraf-Alam25@users.noreply.github.com>
2025-09-18 21:23:38 +0530

    Add files via upload

---
+++ 13 Arrays/ReverseArray.java
@@ -0,0 +1,23 @@
+public class ReverseArray {
+    public static void reverse (int numbers[]){
+        int start = 0, end = numbers.length - 1;
+
+        while(start < end){
+            int temp = numbers[end];
+            numbers[end] = numbers[start];
+            numbers[start] = temp;
+            
+            start ++;
+            end --;
+        }
+        
+    }
+    public static void main(String[] args) {
+        int numbers[] = {2,4,6,33,36,45,68,98,100};
+        reverse(numbers);
+        for(int i=0; i<numbers.length; i++){
+            System.out.print(numbers[i] + " ");
+        }
+        // System.out.println();
+    }
+}
